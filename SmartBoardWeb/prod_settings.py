b9ecb3bc07132c65640e5d9d5f12f74be46fcac1
Hugh Powell <hughpowell2004@yahoo.com.au>
2015-05-17 16:39:18 +1000

    Added production settings file, for different DB backend

---
+++ SmartBoardWeb/prod_settings.py
@@ -0,0 +1,12 @@
+from .settings import *
+
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': 'smartboardweb',                      # Or path to database file if using sqlite3.
+            'USER': 'admin',
+            'PASSWORD': 'secretpassword',
+            'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+            'PORT': '',                      # Set to empty string for default.
+        }
+    }
