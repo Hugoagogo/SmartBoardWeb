40c8f0252096a54ddf7d6db6fe09be6f22c52812
Hugh Powell <hughpowell2004@yahoo.com.au>
2015-05-23 22:41:48 +1000

    Got live updates going

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),  #The Homepage
     url(r'^(?P<channel>[0-9]+)/$', views.channel_page, name='channel'), #Page for the channel
+    url(r'^(?P<channel>[0-9]+)/data/(?:(?P<timestamp>[0-9]+)/)?$', views.channel_data, name='channel_data'), #Page for the channel
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+
+import json
+import datetime
 
 from .models import Channel
 
@@ -12,3 +16,11 @@ def channel_page(request,channel):
     channel = get_object_or_404(Channel,pk=channel)
     return render(request,"channel_page.html",
                   {"channel":channel}) #dictionary object that is used for refference in template
+
+def channel_data(request,channel,timestamp=None):
+    channel = get_object_or_404(Channel,pk=channel)
+    readings = channel.points.all()
+    if timestamp != None:
+        dtime = datetime.datetime.fromtimestamp(float(timestamp))
+        readings = readings.filter(channel=channel,datetime__gt=dtime)
+    return HttpResponse(json.dumps([(reading.datetime.isoformat(),reading.value) for reading in readings]))
