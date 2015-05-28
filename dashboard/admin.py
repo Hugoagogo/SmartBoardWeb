3c32afe5006eb915dc0f0b073b3a69cc353a73de
Hugh Powell <hughpowell2004@yahoo.com.au>
2015-05-28 23:13:04 +1000

    Improved admin setup

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -2,6 +2,33 @@ from django.contrib import admin
 
 from . import models
 
-admin.site.register(models.SmartBoard)
-admin.site.register(models.Channel)
-admin.site.register(models.PowerReading)
+@admin.register(models.SmartBoard)
+class SmartBoardAdmin(admin.ModelAdmin):
+    list_display=('name','get_channel_count')
+    
+    def get_channel_count(self, obj):
+        return obj.channels.count()
+    get_channel_count.short_description = "Channel Count"
+
+@admin.register(models.Channel)
+class ChannelAdmin(admin.ModelAdmin):
+    list_display=('name','get_board_name','channel_num','get_point_count')
+    list_filter = ('board',)
+    
+    def get_board_name(self, obj):
+        return obj.board.name
+    get_board_name.short_description = "Board Name"
+    
+    def get_point_count(self, obj):
+        return obj.points.count()
+    get_point_count.short_description = "Point Count"
+    
+    
+@admin.register(models.PowerReading)
+class PowerReadingAdmin(admin.ModelAdmin):
+    list_display=('get_channel_name','datetime','value')
+    list_filter=('channel',)
+    
+    def get_channel_name(self, obj):
+        return obj.channel.name
+    get_channel_name.short_description = "Channel Name"
