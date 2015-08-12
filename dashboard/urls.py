9b4ef2cc1edc277c6ccc2c498b6d749a891e8995
dk105 <dk105@uowmail.edu.au>
2015-08-12 13:42:23 +1000

    Added Last 1000 Points

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,4 +5,4 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),  #The Homepage
     url(r'^(?P<channel>[0-9]+)/(?P<timeframe>\b5minutes\b|\bhour\b|\bday\b|\b1000points\b)?$', views.channel_page, name='channel'), #Page for the channel
     url(r'^(?P<channel>[0-9]+)/data/(?:(?P<timestamp>[0-9]+)/)?$', views.channel_data, name='channel_data'), #Page for the channel
-]
+] 
